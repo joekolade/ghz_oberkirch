6a32de6c4caf46a5d7086ac1213820104f3acef3
Joe Schäfer <schaefer@scw.info>
2017-10-05 13:31:27 +0200

    News

--- Resources/Template Development/app/scripts/main.js
+++ Resources/Template Development/app/scripts/main.js
@@ -1,3 +1,27 @@
+(function($){
+
+	$.fn.shuffle = function() {
+
+		var allElems = this.get(),
+			getRandom = function(max) {
+				return Math.floor(Math.random() * max);
+			},
+			shuffled = $.map(allElems, function(){
+				var random = getRandom(allElems.length),
+					randEl = $(allElems[random]).clone(true)[0];
+				allElems.splice(random, 1);
+				return randEl;
+			});
+
+		this.each(function(i){
+			$(this).replaceWith($(shuffled[i]));
+		});
+
+		return $(shuffled);
+
+	};
+
+})(jQuery);
 
 $(function(){
    // insert menü into mobile
@@ -19,10 +43,21 @@ $(function(){
 					autoplay: true,
                     autoplaySpeed: 5500,
 					speed: 1220
-                }
+                },
+				maxItems = 5
             ;
 
             if($slider.length){
+
+            	// Shuffle images
+				$slider.find('.slide').shuffle();
+				$slider.find('.slide').each(function () {
+					if($(this).index() >= maxItems){
+						$(this).remove();
+					}
+				});
+
+				// Init slick
 				$slider.slick(slickOptions);
             }
 		},
